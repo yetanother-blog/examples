359d91b9753b3fe807bbb40fcd673831e74cb803
Henrik Fricke <henrikfricke@web.de>
2021-03-22 17:43:44 +0100

    fix(dark mode): dont persist something by default

--- dark-mode-toggle/main.js
+++ dark-mode-toggle/main.js
@@ -2,10 +2,9 @@ const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 const toggle = document.querySelector('.toggle');
 const html = document.querySelector('html');
 
-localStorage.dark ??= prefersDarkMode.matches;
-html.dataset.dark = localStorage.dark;
+html.dataset.dark = localStorage.dark || prefersDarkMode.matches;
 
 toggle.addEventListener('click', () => { 
-  localStorage.dark = !(localStorage.dark == 'true');
+  localStorage.dark = !(html.dataset.dark == 'true');
   html.dataset.dark = localStorage.dark;
 });
